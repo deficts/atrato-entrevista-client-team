9371ac7ecdc689b07cef5073941028fba9e52825
deficts <diegoar7@live.com>
2022-01-15 11:48:35 -0600

    Adding button to project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Button from './components/button/button'
+
+function App() {
+  return (
+    <div>
+      <Button type='ghost'>Ghost</Button>
+      <Button type='primary'>Primary</Button>
+      <Button type='danger'>Danger</Button>
+    </div>
+  );
+}
+
+export default App;
