d06cd3eb4c89b45a6d3edd4cd6578b2f7930da0e
Diego <diegoar7@live.com>
2022-01-17 19:23:34 -0600

    added disabled property to button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,26 +1,44 @@
-import React, { ReactNode } from 'react'
-import './button.css'
+import React, { ReactNode } from 'react';
+import './button.css';
 
-const Button = ({children, type}: {children: ReactNode, type: 'primary' | 'ghost' | 'danger'}) => {
-
-    const getClassName = (type: 'primary' | 'ghost' | 'danger') => {
-        switch(type){
-            case 'primary':
-                return 'button-primary';
-            case 'ghost':
-                return 'button-ghost';
-            case 'danger':
-                return 'button-danger';
-            default:
-                return 'button-primary';
-        }
+const Button = ({
+  children,
+  type,
+  onClick,
+  style,
+  disabled
+}: {
+  children: ReactNode;
+  type: 'primary' | 'ghost' | 'danger';
+  onClick?: any;
+  style?: any;
+  disabled?: boolean
+}) => {
+  const getClassName = (type: 'primary' | 'ghost' | 'danger') => {
+    switch (type) {
+      case 'primary':
+        return 'button-primary';
+      case 'ghost':
+        return 'button-ghost';
+      case 'danger':
+        return 'button-danger';
+      default:
+        return 'button-primary';
     }
+  };
 
-    return (
-        <button className={getClassName(type)}>
-            {children}
-        </button>
-    )
-}
+  return (
+    <button
+      className={getClassName(type)}
+      onClick={() => {
+        onClick();
+      }}
+      style={style}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+};
 
 export default Button;
