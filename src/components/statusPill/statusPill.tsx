462aa24c6420ac24677dab2b7ba9b83e967a3bb6
Diego <diegoar7@live.com>
2022-01-18 23:03:57 -0600

    change status wording

--- src/components/statusPill/statusPill.tsx
+++ src/components/statusPill/statusPill.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import './statusPill.css'
 
-const StatusPill = ({type, style}: {type: 'pendiente' | 'aprobado' | 'rechazado', style?: any}) => {
+const StatusPill = ({type, style}: {type: 'pendiente' | 'completado' | 'en proceso', style?: any}) => {
 
     const getTextFromType = () => {
         switch(type){
-            case 'aprobado':
-                return 'Aprobado';
-            case 'rechazado':
-                return 'Rechazado';
+            case 'completado':
+                return 'Completado';
+            case 'en proceso':
+                return 'En Proceso';
             case 'pendiente':
                 return 'Pendiente';
             default:
@@ -17,7 +17,7 @@ const StatusPill = ({type, style}: {type: 'pendiente' | 'aprobado' | 'rechazado'
     }
 
     return (
-        <div className={`pill ${type}`} style={style}>
+        <div className={`pill ${type === 'en proceso' ? 'en-proceso' : type}`} style={style}>
             {getTextFromType()}
         </div>
     )
